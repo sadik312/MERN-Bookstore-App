b6f1eae0e11deef4aad15c4f58a608c95064e609
sadik312 <sadikislam46@gmail.com>
2024-02-11 23:11:49 +0000

    creating http get route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/',  (request, response) => {
+    console.log(request)
+    return response.status(234).send("Welcome to the MERN Stack app")
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`);
 });
