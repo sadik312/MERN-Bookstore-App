9d110465b16697657eb0eb938ad17cbff5d1be85
sadik312 <sadikislam46@gmail.com>
2024-02-12 22:47:55 +0000

    created route to save a new book with try catch

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,24 @@ app.get('/',  (request, response) => {
     return response.status(234).send("Welcome to the MERN Stack app")
 });
 
+// Route to save a new book
+app.post('/books', async (request, response) => {
+    try{
+        // Validation for input
+        if(
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
 
 // Connect to DB
 mongoose
@@ -17,7 +35,7 @@ mongoose
         // Server to only run if db connection is successful
         app.listen(PORT, () => {
             console.log(`App is listening on port: ${PORT}`);
-});
+        });
     })
     .catch((error) => {
         console.log(error);
