45afded84e3ea16223c2a56bde0bc8037d71837b
sadik312 <sadikislam46@gmail.com>
2024-02-12 22:36:39 +0000

    connected to DB, server to only run if db connection is successful

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 const app = express();
 
 app.get('/',  (request, response) => {
@@ -8,6 +8,17 @@ app.get('/',  (request, response) => {
     return response.status(234).send("Welcome to the MERN Stack app")
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`);
+
+// Connect to DB
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database");
+        // Server to only run if db connection is successful
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`);
 });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
