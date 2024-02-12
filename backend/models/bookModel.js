f73923986f801c117aa2d94ad397546f5693183d
sadik312 <sadikislam46@gmail.com>
2024-02-12 22:42:22 +0000

    created book model in model folder

---
+++ backend/models/bookModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// Create book schema variable
+const bookSchema = mongoose.Schema(
+    {
+        // objects
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        publishYear: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+// Create a book model
+export const Book = mongoose.model('Cat', { name: String });
