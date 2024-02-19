df3d4c9de97b4ee5fe56bd80dd143ea8a1ff7b10
sadik312 <sadikislam46@gmail.com>
2024-02-19 16:11:20 +0000

    imported cors for backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { Book } = require('./models/bookModel.js');
 const booksRoute = require('./routes/booksRoute.js').router;
+const cors = require('cors');
 
 require('dotenv').config();
 
