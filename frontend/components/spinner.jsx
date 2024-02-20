9ac693b001bc5b55de05c235ddb4da194646230a
sadik312 <sadikislam46@gmail.com>
2024-02-20 21:25:00 +0000

    fixed export

--- frontend/components/spinner.jsx
+++ frontend/components/spinner.jsx
@@ -4,8 +4,8 @@ const spinner = () => {
   return (
     <div className='animate-ping w-16 h-16 w-8 rounded-full bg-sky-600'></div>
   )
-}
+};
 
-module.exports {
+module.exports = {
     spinner
-}
+};
