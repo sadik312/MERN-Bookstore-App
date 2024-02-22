1acffb797865dfd8e2bcb770a87f173dfa5329be
sadik312 <sadikislam46@gmail.com>
2024-02-22 22:38:33 +0000

    set up axios request route

--- frontend/components/BackButton.jsx
+++ frontend/components/BackButton.jsx
@@ -5,9 +5,13 @@ import { BsArrowLeft } from 'react-icons/bs';
 
 import React from 'react'
 
-const BackButton = () => {
+const BackButton = ({ destination = '/' }) => {
   return (
-    <div>BackButton</div>
+    <div className='flex'>
+        <Link to={destination} className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'>
+            <BsArrowLeft className='text-2xl' />
+        </Link>
+    </div>
   )
 }
 
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButton from '../../components/BackButton';
+import Spinner from '../../components/spinner'
 
 const ShowBook = () => {
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setBook(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  })
+
   return (
-    <div>ShowBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Show Book</h1>
+    </div>
   )
 }
 
