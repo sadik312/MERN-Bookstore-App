b40bdc78bf1866fc4cbc4535a4f0bf2a77f034cf
sadik312 <sadikislam46@gmail.com>
2024-02-22 22:16:16 +0000

    creating backbutton component

---
+++ frontend/components/BackButton.jsx
@@ -0,0 +1,14 @@
+// Reusable component creating a button to back to the bookslist
+
+import { Link } from 'react-router-dom';
+import { BsArrowLeft } from 'react-icons/bs';
+
+import React from 'react'
+
+const BackButton = () => {
+  return (
+    <div>BackButton</div>
+  )
+}
+
+export default BackButton
