4ff75c37682c7145c0b5aaa5bf8e5aadd02b4ef6
sadik312 <sadikislam46@gmail.com>
2024-02-22 22:10:10 +0000

    fixed DeleteBook page not showing - due to incorrect path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // rafce to create new ReactArrowFunctionExportComponent
 
 import React from 'react'
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBook from './pages/CreateBook';
 import ShowBook from './pages/ShowBook';
@@ -15,7 +15,7 @@ const App = () => {
       <Route path='/books/create' element={<CreateBook />} />
       <Route path='/books/details/:id' element={<ShowBook />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/book/delete/:id' element={<DeleteBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
   );
 };
