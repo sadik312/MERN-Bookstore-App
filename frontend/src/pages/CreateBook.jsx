a9ba0b8c47dfbdbaebd958a1b765c7731c72d466
sadik312 <sadikislam46@gmail.com>
2024-02-24 23:18:52 +0000

    setting states, creating function to handle book create operation for CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../../components/BackButton'
+import Spinner from '../../components/spinner'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreateBook = () => {
+  // states for CreateBook
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  // function to handle book create operation
+  const handleSaveBook = () => {
+    // data object
+    const data = {
+      title,
+      author,
+      publicYear,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error occured, Please check console output');
+      });
+  };
   return (
     <div>CreateBook</div>
   )
