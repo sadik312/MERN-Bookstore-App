b315823ca19f847bcb43f29c23da9cdb5eabc215
sadik312 <sadikislam46@gmail.com>
2024-02-27 23:17:35 +0000

    refactored table view

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import booksTable from '../../components/home/booksTable';
+import booksCard from '../../components/home/booksCard';
 
 const Home = () => {
     const [books, setBooks] = useState([]);
