0840c966186ce927465ec4950cf745f27d31ef2b
sadik312 <sadikislam46@gmail.com>
2024-02-28 21:20:24 +0000

    passed item prop to books

--- frontend/components/home/booksCard.jsx
+++ frontend/components/home/booksCard.jsx
@@ -11,7 +11,7 @@ const BooksCard = ({ books }) => { // receive books as props
     return (
         <div className='grid sm:grid-cols-2 lg:grid:cols-3 xl:grid-cols-4'>
             {books.map((item) => (
-               <BookSingleCard key={item._id} /> 
+               <BookSingleCard key={item._id} books={item} /> 
             ))}
         </div>
     )
