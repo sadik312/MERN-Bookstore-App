2799c2f4b70157c36c77900ca695c7729c82fb50
sadik312 <sadikislam46@gmail.com>
2024-03-12 14:07:10 +0000

    passed id as a prop in backbutton component

--- frontend/components/BackButton.jsx
+++ frontend/components/BackButton.jsx
@@ -5,10 +5,10 @@ import { BsArrowLeft } from 'react-icons/bs';
 
 import React from 'react'
 
-const BackButton = ({ destination = '/' }) => {
+const BackButton = ({ destination = '/', id }) => {
   return (
     <div className='flex'>
-        <Link to={destination} className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'>
+        <Link to={destination} className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit' id={id}>
             <BsArrowLeft className='text-2xl' />
         </Link>
     </div>
