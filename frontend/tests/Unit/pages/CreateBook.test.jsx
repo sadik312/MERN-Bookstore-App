4a461ab9e9d42fd86c5d08dee7f16578298aca0c
sadik312 <sadikislam46@gmail.com>
2024-03-27 22:30:19 +0000

    creating test for CreateBook component

--- frontend/tests/Unit/pages/CreateBook.test.jsx
+++ frontend/tests/Unit/pages/CreateBook.test.jsx
@@ -9,4 +9,25 @@ describe('CreateBook', () => {
             <CreateBook />
         </BrowserRouter>
     });
+
+    test('allows user to input values and submit form', async () => {
+        const { getByLabelText, getByText } = render(
+            <BrowserRouter>
+                <CreateBook />
+            </BrowserRouter>
+        );
+
+        // Get input elements
+        const titleInput = getByLabelText('Title');
+        const authorInput = getByLabelText('Author');
+        const publishYearInput = getByLabelText('Publish Year');
+
+        // Fill in form values
+        fireEvent.change(titleInput, { target: {value : 'Test Title' } });
+        fireEvent.change(authorInput, { target: { value: 'Test Author' } });
+        fireEvent.change(publishYearInput, {target: { value: '2022' } });
+
+        // submit form
+        fireEvent.click(getByLabelText('Save'));
+    })
 });
